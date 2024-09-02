d78e7899e83082ace8d8a26fce53874221703616
Arno Buysse <arno.buysse@swisscom.com>
2024-09-02 14:37:38 +0200

    fixed ui

--- libs/ui/src/components/button/button.component.ts
+++ libs/ui/src/components/button/button.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'sdx-button',
+  selector: 'ui-button',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './button.component.html',
